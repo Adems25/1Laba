501cc96213bfb52a612352de5ffddf1164801e90
Adam <59056332+Adems25@users.noreply.github.com>
2020-12-26 02:43:44 +0300

    1 laba 3 sem

---
+++ L1/L1/L1.cpp
@@ -0,0 +1,220 @@
+﻿#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+class List
+{
+    class Node
+    {
+    public:
+        int key;
+        Node* next;
+        Node(int Key)
+        {
+            key = Key;
+            next = nullptr;
+        }
+    };
+    Node* head;
+    Node* last;
+    size_t size;
+public:
+    List()
+    {
+        head = nullptr;
+        last = nullptr;
+        size = 0;
+    }
+    bool isEmpty()
+    {
+        return (size == 0);
+    }
+    size_t get_size()
+    {
+        return size;
+    }
+    void push_back(int Key)
+    {
+        Node* Include = new Node(Key);
+        if (isEmpty())
+            head = Include;
+        else
+            last->next = Include;
+        last = Include;
+        size++;
+    }
+    void push_front(int Key)
+    {
+        Node* Include = new Node(Key);
+        if (isEmpty())
+            last = Include;
+        else
+            Include->next = head;
+        head = Include;
+        size++;
+    }
+    void pop_back()
+    {
+        if (isEmpty())
+            throw runtime_error("List is empty\n");
+        if (head == last)
+        {
+            delete head;
+            head = nullptr;
+            last = nullptr;
+        }
+        else
+        {
+            Node* Cur = head;
+            while (Cur->next != last)
+                Cur = Cur->next;
+            Cur->next = nullptr;
+            delete last;
+            last = Cur;
+        }
+        size--;
+    }
+    void pop_front()
+    {
+        if (isEmpty())
+            throw runtime_error("List is empty\n");
+        if (head == last)
+        {
+            delete head;
+            head = nullptr;
+            last = nullptr;
+        }
+        else
+        {
+            Node* NewHead = head->next;
+            delete head;
+            head = NewHead;
+        }
+        size--;
+    }
+    void insert(size_t index, int Key)
+    {
+        if (index >= size)
+            throw out_of_range("List is too short\n");
+        if (index == 0)
+            push_front(Key);
+        else
+        {
+            Node* Include = new Node(Key);
+            Node* Cur = head;
+            Node* Prev = nullptr;
+            for (int i = 0;i < index;i++)
+            {
+                Prev = Cur;
+                Cur = Cur->next;
+            }
+            Prev->next = Include;
+            Include->next = Cur;
+            size++;
+        }
+    }
+    int at(size_t index)
+    {
+        if (index >= size)
+            throw out_of_range("List is too short\n");
+        if (index == 0)
+            return head->key;
+        else if (index == (size - 1))
+            return last->key;
+        else
+        {
+            Node* Cur = head;
+            for (int i = 0;i < index;i++)
+                Cur = Cur->next;
+            return Cur->key;
+        }
+    }
+    void remove(size_t index)
+    {
+        if (index >= size)
+            throw out_of_range("List is too short\n");
+        if (index == 0)
+            pop_front();
+        else if (index == (size - 1))
+            pop_back();
+        else
+        {
+            Node* Cur = head;
+            Node* Prev = nullptr;
+            for (int i = 0;i < index;i++)
+            {
+                Prev = Cur;
+                Cur = Cur->next;
+            }
+            Prev->next = Cur->next;
+            delete Cur;
+            size--;
+        }
+    }
+    void set(size_t index, int Key)
+    {
+        if (index >= size)
+            throw out_of_range("List is too short\n");
+        else
+        {
+            insert(index, Key);
+            remove(index + 1);
+        }
+    }
+    void reverse()
+    {
+        if (isEmpty())
+            throw runtime_error("List is empty\n");
+        if (size > 1)
+        {
+            Node* Cur = head;
+            Node* Prev = nullptr;
+            Node* Next = nullptr;
+            while (Cur != nullptr)
+            {
+                Next = Cur->next;
+                Cur->next = Prev;
+                Prev = Cur;
+                Cur = Next;
+            }
+            Cur = head;
+            head = last;
+            last = Cur;
+        }
+    }
+    void print_to_console()
+    {
+        if (isEmpty())
+            throw runtime_error("List is empty\n");
+        Node* Cur = head;
+        while (Cur != nullptr)
+        {
+            cout << Cur->key << ' ';
+            Cur = Cur->next;
+        }
+        cout << endl;
+    }
+    void clear()
+    {
+        while (!isEmpty())
+            pop_front();
+    }
+    ~List()
+    {
+        clear();
+    }
+};
+
+int main()
+{
+    List L;
+    L.push_back(1);
+    L.push_back(3);
+    L.insert(1, 2);
+    L.print_to_console();
+    L.reverse();
+    L.print_to_console();
+    L.remove(1);
+    L.print_to_console();
+}
---
+++ L1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,212 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../L1/L1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(push_back)
+		{
+			List L;
+			L.push_back(1);
+			size_t S = 1;
+			Assert::AreEqual(S, L.get_size());
+			L.push_back(2);
+			S++;
+			Assert::AreEqual(S, L.get_size());
+			Assert::AreEqual(L.at(0), 1);
+			Assert::AreEqual(L.at(1), 2);
+		}
+		TEST_METHOD(push_front)
+		{
+			List L;
+			L.push_front(1);
+			size_t S = 1;
+			Assert::AreEqual(S, L.get_size());
+			L.push_front(2);
+			S++;
+			Assert::AreEqual(S, L.get_size());
+			Assert::AreEqual(L.at(0), 2);
+			Assert::AreEqual(L.at(1), 1);
+		}
+		TEST_METHOD(pop_back)
+		{
+			List L;
+			L.push_back(1);
+			L.push_back(2);
+			size_t S = 1;
+			L.pop_back();
+			Assert::AreEqual(S, L.get_size());
+			Assert::AreEqual(L.at(0), 1);
+			L.pop_back();
+			S--;
+			Assert::AreEqual(S, L.get_size());
+		}
+		TEST_METHOD(pop_back_except)
+		{
+			List L;
+			bool b = 0;
+			try
+			{
+				L.pop_back();
+			}
+			catch (exception& exception)
+			{
+				b = 1;
+			}
+			Assert::IsTrue(b);
+		}
+		TEST_METHOD(pop_front)
+		{
+			List L;
+			L.push_back(1);
+			L.push_back(2);
+			size_t S = 1;
+			L.pop_front();
+			Assert::AreEqual(S, L.get_size());
+			Assert::AreEqual(L.at(0), 2);
+			L.pop_front();
+			S--;
+			Assert::AreEqual(S, L.get_size());
+		}
+		TEST_METHOD(pop_front_except)
+		{
+			List L;
+			bool b = 0;
+			try
+			{
+				L.pop_back();
+			}
+			catch (exception& exception)
+			{
+				b = 1;
+			}
+			Assert::IsTrue(b);
+		}
+		TEST_METHOD(insert)
+		{
+			List L;
+			L.push_back(1);
+			L.push_back(3);
+			L.insert(1, 2);
+			size_t S = 3;
+			Assert::AreEqual(S, L.get_size());
+			Assert::AreEqual(L.at(0), 1);
+			Assert::AreEqual(L.at(1), 2);
+			Assert::AreEqual(L.at(2), 3);
+		}
+		TEST_METHOD(insert_except)
+		{
+			List L;
+			bool b = 0;
+			try
+			{
+				L.insert(0, 0);
+			}
+			catch (exception& exception)
+			{
+				b = 1;
+			}
+			Assert::IsTrue(b);
+		}
+		TEST_METHOD(at_except)
+		{
+			List L;
+			bool b = 0;
+			try
+			{
+				L.at(0);
+			}
+			catch (exception& exception)
+			{
+				b = 1;
+			}
+			Assert::IsTrue(b);
+		}
+		TEST_METHOD(remove)
+		{
+			List L;
+			L.push_back(1);
+			L.push_back(3);
+			L.insert(1, 2);
+			size_t S = 2;
+			L.remove(1);
+			Assert::AreEqual(S, L.get_size());
+			Assert::AreEqual(L.at(0), 1);
+			Assert::AreEqual(L.at(1), 3);
+	
+		}
+		
+		TEST_METHOD(remove_except)
+		{
+			List L;
+			bool b = 0;
+			try
+			{
+				L.remove(0);
+			}
+			catch (exception& exception)
+			{
+				b = 1;
+			}
+			Assert::IsTrue(b);
+		}
+		TEST_METHOD(set)
+		{
+			List L;
+			L.push_back(1);
+			L.push_back(3);
+			L.insert(1, 2);
+			size_t S = 3;
+			L.set(1, 4);
+			Assert::AreEqual(S, L.get_size());
+			Assert::AreEqual(L.at(0), 1);
+			Assert::AreEqual(L.at(1), 4);
+			Assert::AreEqual(L.at(2), 3);
+		}
+		TEST_METHOD(set_except)
+		{
+			List L;
+			bool b = 0;
+			try
+			{
+				L.set(0, 0);
+			}
+			catch (exception& exception)
+			{
+				b = 1;
+			}
+			Assert::IsTrue(b);
+		}
+		TEST_METHOD(reverse)
+		{
+			List L;
+			L.push_back(1);
+			L.push_back(3);
+			L.insert(1, 2);
+			L.reverse();
+			Assert::AreEqual(L.at(0), 3);
+			Assert::AreEqual(L.at(1), 2);
+			Assert::AreEqual(L.at(2), 1);
+		}
+		TEST_METHOD(reverse_except)
+		{
+			List L;
+			bool b = 0;
+			try
+			{
+				L.reverse();
+			}
+			catch (exception& exception)
+			{
+				b = 1;
+			}
+			Assert::IsTrue(b);
+		}
+	};
+}
